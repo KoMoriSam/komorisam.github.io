1864cb2ff820d041281da894dd7195e34207274f
KoMoriSam <1760665075@qq.com>
2023-09-09 22:22:41 +0800

    👍improved sider.js 23.09.09

--- js/sider.js
+++ js/sider.js
@@ -1,4 +1,4 @@
-/*=============== SHOW SIDEBAR ===============*/
+/*=============== 侧边栏收放 ===============*/
 const showSidebar = (toggleId, sidebarId, mainId) =>{
   const toggle = document.getElementById(toggleId),
   sidebar = document.getElementById(sidebarId),
@@ -6,17 +6,14 @@ const showSidebar = (toggleId, sidebarId, mainId) =>{
 
   if(toggle && sidebar && main){
       toggle.addEventListener('click', ()=>{
-          /* Show sidebar */
           sidebar.classList.toggle('show-sidebar')
-          /* Add padding main */
           main.classList.toggle('main-pd')
-          // toggle.classList.toggle('header-active')
       })
   }
 }
 showSidebar('header-toggle','sidebar', 'main')
 
-/*=============== LINK ACTIVE ===============*/
+/*=============== 激活标签 ===============*/
 const sidebarLink = document.querySelectorAll('.sidebar_link')
 
 function linkColor(){
@@ -26,20 +23,17 @@ function linkColor(){
 
 sidebarLink.forEach(l => l.addEventListener('click', linkColor))
 
-
-$(document).ready(function () {    // 等待页面加载完成
-  var path = window.location.pathname;  // 获取当前页面路径
-  // 根据路径更改导航栏标题背景色
-  if (path === '/' || path === '/index.html') {
-    $('#home').addClass('active-link');
-  } else if (path === '/about.html') {
-    $('#about').addClass('active-link');
-  } else if (path === '/novel.html') {
-    $('#novel').addClass('active-link');
-  } else if (path === '/contact.html') {
-    $('#contact').addClass('active-link');
-  } else if (path === '/test.html') {
-    $('#test').addClass('active-link');
-  }
+$(document).ready(function () {
+  var path = window.location.pathname;
+  var pathToIdMap = {
+    '/': 'home',
+    '/index.html': 'home',
+    '/about.html': 'about',
+    '/novel.html': 'novel',
+    '/contact.html': 'contact',
+    '/test.html': 'test'
+  };
+  var pageId = pathToIdMap[path] || 'default';
+  $('#' + pageId).addClass('active-link');
 });
 
