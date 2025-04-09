fa5eba583bbcd27d7ec1ad114ce40f820e065532
KoMoriSam <1760665075@qq.com>
2025-04-09 20:34:46 +0800

    微调主页中的链接样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,20 +7,18 @@
   >
     <p>在这里会分享一些正在做的小玩意</p>
     <br />
-    <small class="text-sm text-base-content/50">
+    <small class="text-xs lg:text-sm text-base-content/50">
       在找旧版网页？
       <a
-        class="btn btn-primary btn-xs no-underline"
+        class="btn btn-primary btn-xs no-underline mb-px"
         href="/archive/home/index.html"
-        >这里跳转</a
       >
+        这里跳转
+      </a>
     </small>
   </BasePage>
 </template>
 
 <script setup>
 import BasePage from "@/components/layout/BasePage.vue";
-
-import { useRouter } from "vue-router";
-const route = useRouter();
 </script>
