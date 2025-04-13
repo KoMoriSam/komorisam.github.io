80f18b36aa43b38a6346a646f75abd43802d4820
KoMoriSam <1760665075@qq.com>
2025-04-13 15:39:04 +0800

    完善联系页面链接功能

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -6,16 +6,29 @@
     description="可以加 QQ 和 微信 捏~"
   >
     <br />
-    <address class="join max-sm:join-vertical">
+    <address class="join max-sm:join-vertical relative">
       <a
         class="join-item btn btn-info text-info-content no-underline text-left justify-start"
-        ><i class="ri-qq-fill"></i> 3593516798</a
+        href="https://qm.qq.com/q/eYk72ol3qg"
+        target="_blank"
       >
+        <i class="ri-qq-fill"></i> 3593516798
+      </a>
       <a
-        class="join-item btn btn-info text-info-content no-underline text-left justify-start"
-        ><i class="ri-wechat-fill"></i> MoriSam2023</a
+        class="join-item btn btn-info text-info-content no-underline text-left justify-start relative group"
+        href="weixin://dl/scan"
+        target="_blank"
       >
+        <i class="ri-wechat-fill"></i> MoriSam2023
+        <!-- 悬停时显示的图片 -->
+        <img
+          class="card bg-base-100 shadow-xl absolute left-42 p-2 mt-2 opacity-0 w-32 h-32 group-hover:opacity-100 transition-opacity duration-250"
+          src="/assets/images/profile/wechat.png"
+          alt="My WeChat QR Code"
+        />
+      </a>
     </address>
+    <br />
   </BasePage>
 </template>
 
