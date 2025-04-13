82fd70bb4af03daec6febf4a693c0941533c38ea
KoMoriSam <1760665075@qq.com>
2025-04-13 15:19:42 +0800

    新增 `Modal` 组件  实现模态对话框功能

---
+++ src/components/ui/feedback/Modal.vue
@@ -0,0 +1,33 @@
+<template>
+  <dialog
+    id="my_modal_5"
+    class="modal modal-bottom sm:modal-middle"
+    :open="visible"
+  >
+    <section class="modal-box">
+      <h3 class="text-lg font-bold">{{ title }}</h3>
+      <p class="py-4">按下 ESC 键或点击按钮以关闭</p>
+      <form class="modal-action" method="dialog" @submit.prevent="closeModal">
+        <button class="btn">关闭</button>
+      </form>
+    </section>
+  </dialog>
+</template>
+
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    default: "Hello!",
+  },
+  visible: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+const emit = defineEmits(["close"]);
+const closeModal = () => {
+  emit("close");
+};
+</script>
