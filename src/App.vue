1ffb0054d286c1473e3abe9fcb199a00c0f859f2
KoMoriSam <1760665075@qq.com>
2025-04-14 22:58:19 +0800

    新增大版本更新清除遗弃缓存功能

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <NavBar />
   <router-view />
+  <BigUpdate />
 </template>
 
 <script setup>
 import NavBar from "@/components/layout/NavBar.vue";
+import BigUpdate from "@/components/base/BigUpdate.vue";
 </script>
---
+++ src/components/base/BigUpdate.vue
@@ -0,0 +1,23 @@
+<template>
+  <Modal
+    :visible="showModal"
+    @close="handleModalClose"
+    title="我们有个大更新！"
+    description="本次更新将会清除以往的阅读数据，请确认以继续。"
+    buttonText="确认"
+  />
+</template>
+
+<script setup>
+import Modal from "@/components/ui/feedback/Modal.vue";
+
+import { useStorage } from "@vueuse/core";
+
+import { useCleanStorage } from "@/utils/cleanStorage";
+
+const showModal = useStorage("IS_NOT_UPDATED", true);
+const handleModalClose = () => {
+  showModal.value = false;
+  useCleanStorage();
+};
+</script>
