084e59141105b626a0e5cd05f950b37c7add4c27
KoMoriSam <1760665075@qq.com>
2025-05-19 16:20:41 +0800

    Updates

---
+++ js/Loading-DiJra7W8.js
@@ -0,0 +1,1 @@
+import{a as n}from"./ToTop-BZELpEwZ.js";import{n as s,o,a as t}from"./index-CMZBC3qO.js";const a={},r={class:"my-48 flex items-center justify-center"};function c(i,e){return o(),s("main",r,e[0]||(e[0]=[t("span",{class:"loading loading-spinner loading-xl text-primary"},null,-1)]))}const d=n(a,[["render",c]]);export{d as L};
