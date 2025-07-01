bf8713556c6c134a62ed1a76699113acaee1923e
KoMoriSam <1760665075@qq.com>
2025-07-01 23:48:03 +0800

    优化 Loading 组件，支持动态大小属性

--- src/components/base/Loading.vue
+++ src/components/base/Loading.vue
@@ -1,5 +1,14 @@
 <template>
-  <main class="my-48 flex items-center justify-center">
+  <main class="flex items-center justify-center" :class="size">
     <span class="loading loading-spinner loading-xl text-primary"></span>
   </main>
 </template>
+
+<script setup>
+defineProps({
+  size: {
+    type: String,
+    default: "",
+  },
+});
+</script>
