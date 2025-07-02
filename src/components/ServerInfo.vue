c97d180faeac4503bd295f743de5cf8b998a0a5d
KoMoriSam <1760665075@qq.com>
2025-07-02 13:23:52 +0800

    修复服务器信息组件在默认亮色主题下的配色错误

--- src/components/ServerInfo.vue
+++ src/components/ServerInfo.vue
@@ -1,10 +1,10 @@
 <template>
   <section v-if="serverInfo" class="prose prose-sm">
     <div
-      class="stats stats-vertical lg:stats-horizontal bg-primary-content text-primary shadow"
+      class="stats stats-vertical lg:stats-horizontal bg-info-content text-info shadow"
     >
       <div class="stat">
-        <div class="stat-title">
+        <div class="stat-title text-info/75">
           <i class="ri-hard-drive-3-line"></i>
           服务器
         </div>
@@ -17,10 +17,10 @@
             v-fade-in
           />
         </div>
-        <div class="stat-desc">{{ serverInfo.city }}</div>
+        <div class="stat-desc text-info/75">{{ serverInfo.city }}</div>
       </div>
       <div class="stat">
-        <div class="stat-title">
+        <div class="stat-title text-info/75">
           <div
             v-if="serverInfo.ping === null"
             class="inline-grid *:[grid-area:1/1]"
@@ -37,18 +37,18 @@
         <div class="stat-value my-5">
           {{ serverInfo.ping === null ? "离线" : "在线" }}
         </div>
-        <div class="stat-desc">{{ serverInfo.motd }}</div>
+        <div class="stat-desc text-info/75">{{ serverInfo.motd }}</div>
       </div>
 
       <div class="stat" v-if="serverInfo.ping !== null">
-        <div class="stat-title">
+        <div class="stat-title text-info/75">
           <i class="ri-user-3-line"></i>
           在线人数
         </div>
         <div class="stat-value my-5">
           {{ serverInfo.p }} / {{ serverInfo.mp }}
         </div>
-        <div class="stat-desc">
+        <div class="stat-desc text-info/75">
           <span
             class="badge badge-sm badge-error"
             v-if="serverInfo.p / serverInfo.mp >= 0.75"
@@ -66,12 +66,12 @@
       </div>
 
       <div class="stat" v-if="serverInfo.ping !== null">
-        <div class="stat-title">
+        <div class="stat-title text-info/75">
           <i class="ri-timer-line"></i>
           网络延迟
         </div>
         <div class="stat-value my-5">{{ serverInfo.ping }} ms</div>
-        <div class="stat-desc">
+        <div class="stat-desc text-info/75">
           <span
             class="badge badge-sm badge-error"
             v-if="serverInfo.ping > 200 || serverInfo.ping === null"
