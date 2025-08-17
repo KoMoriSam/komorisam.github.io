a88af35b4eda779b194dd9c0582b13caa70dceab
KoMoriSam <1760665075@qq.com>
2025-08-17 19:17:51 +0800

    抽取@/components/UpdateDetail组件，简化@/utils/update-notice.js代码

---
+++ src/components/UpdateDetail.vue
@@ -0,0 +1,82 @@
+<template>
+  <section class="prose prose-sm">
+    <h2 class="text-primary font-black">
+      {{ props.version }}
+      <span class="badge badge-xs text-base-content/50 ml-1">
+        {{ props.releaseDate }}
+      </span>
+      <div class="dropdown dropdown-center dropdown-hover">
+        <div role="button" class="btn btn-circle btn-ghost btn-xs text-info">
+          <i class="ri-information-line"></i>
+        </div>
+        <div
+          class="card card-sm dropdown-content bg-base-100 rounded-box z-1 w-48 shadow-sm"
+        >
+          <section class="card-body">
+            <h2 class="card-title mt-0 mb-0">想查看详细更新日志？</h2>
+            <a
+              href="/changelog"
+              @click.stop="handleViewLog"
+              class="link link-primary no-underline"
+            >
+              点击此处<i class="ri-arrow-right-up-line"></i>
+            </a>
+          </section>
+        </div>
+      </div>
+    </h2>
+
+    <ul class="list-none p-0">
+      <template
+        v-for="([type, descriptions], typeIndex) in Object.entries(
+          props.changelog
+        )"
+        :key="typeIndex"
+      >
+        <li
+          v-for="(description, descIndex) in descriptions"
+          :key="`${type}-${descIndex}`"
+        >
+          <strong :class="`badge badge-soft badge-sm ${typeColor(type)}`">
+            {{ typeText(type) }}
+          </strong>
+          {{ description }}
+        </li>
+      </template>
+    </ul>
+
+    <p v-if="props.migration" class="mt-3 text-sm">
+      <span
+        :class="[
+          'badge badge-xs',
+          props.warning ? 'badge-warning' : 'badge-success',
+        ]"
+      >
+        <i :class="props.warning ? 'ri-alert-line' : 'ri-check-line'"></i>
+        {{ props.warning ? "请注意" : "放心食用" }}
+      </span>
+      {{ props.note || props.warning }}
+    </p>
+  </section>
+</template>
+
+<script setup>
+const props = defineProps({
+  version: String,
+  releaseDate: String,
+  changelog: {
+    type: Object,
+    required: true,
+  },
+  migration: Boolean,
+  warning: String,
+  note: String,
+  onViewLog: Function,
+});
+
+const handleViewLog = (e) => {
+  props.onViewLog?.();
+};
+
+import { typeColor, typeText } from "@/utils/type-changelog";
+</script>
