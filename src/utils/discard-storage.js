d19db62e9d160a71d595e00150432289dcd79563
KoMoriSam <1760665075@qq.com>
2025-08-17 17:25:02 +0800

    增加新的无效Storage键值至清理列表

--- src/utils/discard-storage.js
+++ src/utils/discard-storage.js
@@ -1,10 +1,27 @@
 import { useStorage } from "@vueuse/core";
 
 export function useDiscardStorage() {
-  const keysToClear = ["READ_CHAPTER", "READ_CH", "IS_NOT_UPDATED"];
+  const keysToClear = [
+    "READ_CHAPTER",
+    "READ_CH",
+    "IS_NOT_UPDATED",
+    "READ_HEADING",
+    "nowChapter",
+    "auto",
+    "mode",
+    "sidebarState",
+  ];
 
   keysToClear.forEach((key) => {
     const storageItem = useStorage(key, null);
     storageItem.value = null;
+  });
+
+  // 清除所有 chapter_XXX 格式的键
+  Object.keys(localStorage).forEach((key) => {
+    if (key.startsWith("chapter_")) {
+      const storageItem = useStorage(key, null);
+      storageItem.value = null;
+    }
   });
 }
